2a0ac7a4204ae53aa157282f4f632f33078d0363
nguyen <daudaua3782@gmail.com>
2020-08-26 09:58:09 +0700

    Edit RPG

--- src/main/java/com/playernguyen/asset/gun/RocketGun.java
+++ src/main/java/com/playernguyen/asset/gun/RocketGun.java
@@ -4,6 +4,7 @@ import com.playernguyen.Weaponist;
 import com.playernguyen.entity.Shooter;
 import com.playernguyen.ray.RayResult;
 import com.playernguyen.runnable.RocketProjectileRunnable;
+import com.playernguyen.sound.SoundConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -31,10 +32,14 @@ public abstract class RocketGun extends DefaultGun {
         r.runTaskTimerAsynchronously(Weaponist.getWeaponist(), 0, 0);
 
         // Take ammo
-        shooter
-                .getCurrentItem()
+        player
+                .getInventory().getItemInMainHand()
                 .setAmount(shooter.getCurrentItem().getAmount() - 1);
 
+        // Play sound
+        for (SoundConfiguration soundConfiguration : getShootSoundList()) {
+            soundConfiguration.play(shooter.asPlayer().getLocation());
+        }
         return null;
     }
 }
